a8b693965f3838cfc128cbbf8f8cff482cd08a0a
malloryandalphaone <42677997+malloryandalphaone@users.noreply.github.com>
2019-05-14 07:16:31 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,19 +14,22 @@ client.on("ready", () => {
 client.user.setStatus('dnd');
 //client.user.setGame("System", "https://www.twitch.tv/idk");
   console.log("Rank | Logged in! Server count: ${client.guilds.size}");
-  client.user.setActivity("Rank System.",{type: 'PLAYING'});
+  client.user.setActivity("Ranks.",{type: 'PLAYING'});
 });
 
 
 client.on("message", message => {
     const Tr = client.emojis.find(load => load.name === "trn");
     const trs = client.emojis.find(load => load.name === "trn");
+    const P = client.emojis.find(load => load.name === "plus");
     if(message.content.startsWith(prefix + "trial")) {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`\`-\` **You don't have permissions,** ${trs}`);
     if (!message.content.split(" ").slice(1)[0]) return message.channel.send(`${Tr} **Mention any member.**`);
 
     let target = message.mentions.members.first();
     if (!target) return message.channel.send(`${Tr} **Invalid mention**`);
+      
+      message.channel.send(`${P} **Give a role for** ${target}`);
 
     let role = message.guild.roles.find(role => role.name === "- Trial Admin ♪");
     target.addRole(role);
@@ -36,12 +39,15 @@ client.on("message", message => {
 client.on("message", message => {
     const Tr = client.emojis.find(load => load.name === "trn");
     const trs = client.emojis.find(load => load.name === "trn");
+    const P = client.emojis.find(load => load.name === "plus");
     if(message.content.startsWith(prefix + "exp")) {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`\`-\` **You don't have permissions,** ${trs}`);
     if (!message.content.split(" ").slice(1)[0]) return message.channel.send(`${Tr} **Mention any member.**`);
 
     let target = message.mentions.members.first();
     if (!target) return message.channel.send(`${Tr} **Invalid mention**`);
+      
+      message.channel.send(`${P} **Give a role for** ${target}`);
 
     let role = message.guild.roles.find(role => role.name === "- Expert.");
     target.addRole(role);
@@ -51,12 +57,15 @@ client.on("message", message => {
 client.on("message", message => {
     const Tr = client.emojis.find(load => load.name === "trn");
     const trs = client.emojis.find(load => load.name === "trn");
+    const P = client.emojis.find(load => load.name === "plus");
     if(message.content.startsWith(prefix + "not ver")) {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`\`-\` **You don't have permissions,** ${trs}`);
     if (!message.content.split(" ").slice(1)[0]) return message.channel.send(`${Tr} **Mention any member.**`);
 
     let target = message.mentions.members.first();
     if (!target) return message.channel.send(`${Tr} **Invalid mention**`);
+      
+      message.channel.send(`${P} **Give a role for** ${target}`);
 
     let role = message.guild.roles.find(role => role.name === "- Not Verified.");
     target.addRole(role);
@@ -66,12 +75,15 @@ client.on("message", message => {
 client.on("message", message => {
     const Tr = client.emojis.find(load => load.name === "trn");
     const trs = client.emojis.find(load => load.name === "trn");
+    const P = client.emojis.find(load => load.name === "plus");
     if(message.content.startsWith(prefix + "active")) {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`\`-\` **You don't have permissions,** ${trs}`);
     if (!message.content.split(" ").slice(1)[0]) return message.channel.send(`${Tr} **Mention any member.**`);
 
     let target = message.mentions.members.first();
     if (!target) return message.channel.send(`${Tr} **Invalid mention**`);
+      
+      message.channel.send(`${P} **Give a role for** ${target}`);
 
     let role = message.guild.roles.find(role => role.name === "- Professional Active.");
     target.addRole(role);
@@ -81,12 +93,15 @@ client.on("message", message => {
 client.on("message", message => {
     const Tr = client.emojis.find(load => load.name === "trn");
     const trs = client.emojis.find(load => load.name === "trn");
+    const P = client.emojis.find(load => load.name === "plus");
     if(message.content.startsWith(prefix + "fami")) {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`\`-\` **You don't have permissions,** ${trs}`);
     if (!message.content.split(" ").slice(1)[0]) return message.channel.send(`${Tr} **Mention any member.**`);
 
     let target = message.mentions.members.first();
     if (!target) return message.channel.send(`${Tr} **Invalid mention**`);
+      
+      message.channel.send(`${P} **Give a role for** ${target}`);
 
     let role = message.guild.roles.find(role => role.name === "- Professional Family.");
     target.addRole(role);
@@ -96,12 +111,15 @@ client.on("message", message => {
 client.on("message", message => {
     const Tr = client.emojis.find(load => load.name === "trn");
     const trs = client.emojis.find(load => load.name === "trn");
+    const P = client.emojis.find(load => load.name === "plus");
     if(message.content.startsWith(prefix + "deny")) {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`\`-\` **You don't have permissions,** ${trs}`);
     if (!message.content.split(" ").slice(1)[0]) return message.channel.send(`${Tr} **Mention any member.**`);
 
     let target = message.mentions.members.first();
     if (!target) return message.channel.send(`${Tr} **Invalid mention**`);
+      
+      message.channel.send(`${P} **Give a role for** ${target}`);
 
     let role = message.guild.roles.find(role => role.name === "Clan denied.");
     target.addRole(role);
@@ -111,12 +129,15 @@ client.on("message", message => {
 client.on("message", message => {
     const Tr = client.emojis.find(load => load.name === "trn");
     const trs = client.emojis.find(load => load.name === "trn");
+    const P = client.emojis.find(load => load.name === "plus");
     if(message.content.startsWith(prefix + "ment")) {
     if(!message.member.hasPermission('MANAGE_ROLES')) return message.channel.send(`\`-\` **You don't have permissions,** ${trs}`);
     if (!message.content.split(" ").slice(1)[0]) return message.channel.send(`${Tr} **Mention any member.**`);
 
     let target = message.mentions.members.first();
     if (!target) return message.channel.send(`${Tr} **Invalid mention**`);
+      
+      message.channel.send(`${P} **Give a role for** ${target}`);
 
     let role = message.guild.roles.find(role => role.name === "Mention.");
     target.addRole(role);
